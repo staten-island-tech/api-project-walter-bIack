c8ed9ba5bd627d51c459be3303fe491f15a71d48
walter-bIack <leon.selyomin@gmail.com>
2023-02-09 12:48:41 -0500

    5

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,41 +2,73 @@ import "../styles/style.css";
 import { list } from "./list";
 import { DOMSelectors } from "./dom";
 
-DOMSelectors.dogwater.addEventListener("click", () => createDisplay("dogwater"));
+let Q = 0;
+let D = 0;
+let N = 0;
+let P = 0;
+let C = 155;
+
+while (C >= 1) {
+  if (C >= 25) {
+    C -= 25;
+    Q++; // 6
+  }
+  if (C <= 24 && C >= 10) {
+    C -= 10;
+    D++;
+  }
+  if (C <= 9 && C >= 5) {
+    C -= 5;
+    N++; // 1
+  }
+  if (C <= 4 && C >= 1) {
+    C -= 1;
+    P++;
+  }
+}
+console.log(C);
+console.log(Q);
+console.log(D);
+console.log(N);
+console.log(P);
+
+DOMSelectors.dogwater.addEventListener("click", () =>
+  createDisplay("dogwater")
+);
 
 DOMSelectors.mid.addEventListener("click", () => createDisplay("mid"));
 DOMSelectors.good.addEventListener("click", () => createDisplay("good"));
 
-document.body.onload = () => {createDisplay("dogwater")}
+document.body.onload = () => {
+  createDisplay("dogwater");
+};
 
 const form = document.getElementById("form1");
-form.addEventListener("submit", async function(event) {
+form.addEventListener("submit", async function (event) {
   event.preventDefault();
-  let temp = await getquote(document.getElementById("input1").value)
+  let temp = await getquote(document.getElementById("input1").value);
   if (temp === true) {
-      alert(`${document.getElementById("input1").value} is not a valid term`)
-
+    alert(`${document.getElementById("input1").value} is not a valid term`);
   } else {
     document.querySelectorAll(".card1").forEach(async (card) => {
-    let data = await getquote(document.getElementById("input1").value)
-    
-    console.log(data)
-    card.innerHTML = ""
-    card.insertAdjacentHTML(
-    "beforeend",
-    `<figcaption class="quote">${data.content}</figcaption>`
-  );})
-    }
+      let data = await getquote(document.getElementById("input1").value);
+
+      console.log(data);
+      card.innerHTML = "";
+      card.insertAdjacentHTML(
+        "beforeend",
+        `<figcaption class="quote">${data.content}</figcaption>`
+      );
+    });
+  }
 });
 
 const getquote = async (input) => {
   try {
-    let response = await fetch(
-      `https://api.quotable.io/random?tags=${input}`
-    );
-    console.log(response.ok)
+    let response = await fetch(`https://api.quotable.io/random?tags=${input}`);
+    console.log(response.ok);
     if (!response.ok) {
-      return true
+      return true;
     }
     let data = await response.json();
     return data;
